b9473e4ebfc6aaf224f36216945940d965311811
Proddy <tehmcterrorist@gmail.com>
2021-09-09 17:32:49 +0100

    v3.3.5-Beta
    - Added a hacky music fix for Reverse Mission Order

--- RewardsRando/Resources/HandleLevelMusic.lua
+++ RewardsRando/Resources/HandleLevelMusic.lua
@@ -1,7 +1,16 @@
-local Path = GetPath()
-local GamePath = GetGamePath(Path)
+if Settings.ReverseMissionOrder and Settings.HackyMusicFix then
+	local Path = GetPath()
+	local GamePath = GetGamePath(Path)
+	local Level = tonumber(Path:match("[lL](%d)"))
 
-local File = ReadFile(GamePath)
-local RMSFile = RMS.File:new(File)
+	local File = ReadFile(GamePath)
+	local RMSFile = RMS.File:new(File)
 
-Output(RMSFile:Output())
+	for i=1,7 do
+		if MissionOrder[Level][i] ~= i then
+			RMSFile.Data = RMSFile.Data:gsub("M"..i, "M"..MissionOrder[Level][i])
+		end
+	end
+
+	Output(RMSFile:Output())
+end
